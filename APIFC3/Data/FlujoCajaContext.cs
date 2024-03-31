355721bc9358e7e36bfe9acbc0df65938965110f
javiergraff890 <jjaviergraff@gmail.com>
2024-03-31 18:17:12 -0300

    saque el string connection del codigo y lo coloque en un archivo de configuracion

--- APIFC3/Data/FlujoCajaContext.cs
+++ APIFC3/Data/FlujoCajaContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using APIFC3.Data.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace APIFC3.Data;
 
@@ -23,8 +24,12 @@ public partial class FlujoCajaContext : DbContext
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=flujo_caja;Trusted_connection=true; TrustServerCertificate=true");
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfiguration configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("flujoCajaString"));
+    }
+       
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
